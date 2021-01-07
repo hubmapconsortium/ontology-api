9f2213d6407e6d1664763227f7aba6bb1a29fa1f
Chuck Borromeo <chb69@pitt.edu>
2021-01-07 17:05:55 -0500

    added a pre processing file for creating the layered source files from the PheKnowLator data

---
+++ src/neo4j_loader/pre_process_files.sh
@@ -0,0 +1,9 @@
+cd ~/umls_data/ontology_files_orig
+grep "^http://purl.obolibrary.org/obo/CL_" PheKnowLator_Subclass_OWLNETS_Ontology_DbXRef_16OCT2020.txt > /home/chb69/umls_data/ontology_files/cl_dbxref.txt
+grep "^http://purl.obolibrary.org/obo/UBERON_" PheKnowLator_Subclass_OWLNETS_Ontology_DbXRef_16OCT2020.txt > /home/chb69/umls_data/ontology_files/uberon_dbxref.txt
+grep "^http://purl.obolibrary.org/obo/CL_" PheKnowLator_Subclass_OWLNETS_edge_list_16OCT2020.txt > /home/chb69/umls_data/ontology_files/cl_edge_list.txt
+grep "^http://purl.obolibrary.org/obo/UBERON_" PheKnowLator_Subclass_OWLNETS_edge_list_16OCT2020.txt > /home/chb69/umls_data/ontology_files/uberon_edge_list.txt
+grep "^http://purl.obolibrary.org/obo/CL_" PheKnowLator_Subclass_OWLNETS_NodeMetadata_16OCT2020.txt > /home/chb69/umls_data/ontology_files/cl_node_metadata.txt
+grep "^http://purl.obolibrary.org/obo/UBERON_" PheKnowLator_Subclass_OWLNETS_NodeMetadata_16OCT2020.txt > /home/chb69/umls_data/ontology_files/uberon_node_metadata.txt
+
+

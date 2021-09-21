7bad69620d8619ac2863befca22261b40a97890f
yuanzhou <yuanzhou19@gmail.com>
2021-09-21 15:12:24 -0400

    Disable UI

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,7 +1,8 @@
 import connexion
 from openapi_server import encoder
 
-app = connexion.App(__name__, specification_dir='openapi_server/openapi/')
+options = {"swagger_ui": False}
+app = connexion.App(__name__, specification_dir='openapi_server/openapi/', options=options)
 
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('openapi.yaml', arguments={'title': 'Ontology API'}, pythonic_params=True)

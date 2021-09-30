fc1171fae1a8413aeda915fdb731647dcd0f13d4
ChuckKollar <chuck@kollar.com>
2021-09-30 12:45:13 -0400

    Added --all to fix_tsv_file.py

--- scripts/owlnets_script/fix_tsv_file.py
+++ scripts/owlnets_script/fix_tsv_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import argparse, os, re, unicodedata, itertools, sys, string
+import argparse, os, re, unicodedata, sys, string
 
 
 class RawTextArgumentDefaultsHelpFormatter(
@@ -15,11 +15,15 @@ parser = argparse.ArgumentParser(
     description='Post process tsv files produced py PheKnowLator',
     formatter_class=RawTextArgumentDefaultsHelpFormatter)
 parser.add_argument('filename', type=str,
-                    help='file to fix')
+                    help='file or directory to fix')
 parser.add_argument("-c", "--check", action="store_true",
-                    help='check that all records have the same number of fields')
+                    help='check that all records have the same number of fields; exit status 0 when no bad records,'
+                         ' else 1')
 parser.add_argument("-f", "--fix", type=str,
-                    help='for records with fewer fields, append lines till the field count is correct')
+                    help='for records with fewer fields, append lines till the field count is correct;'
+                         ' then do an implicit --check on the output file')
+parser.add_argument("-a", "--all", action="store_true",
+                    help='fix all OWLNETS_node_metadata.txt files in the filename which should be a directory')
 args = parser.parse_args()
 
 
@@ -34,7 +38,8 @@ def count_of_char_in_str(ch: int, s: str) -> int:
     return lst.count(ch)
 
 
-def check(ifilename: str) -> None:
+def check(ifilename: str) -> int:
+    print(f"Checking file {ifilename}...")
     file_r = open(ifilename, 'r')
 
     # Process the header to determine the number of fields in a record...
@@ -56,8 +61,9 @@ def check(ifilename: str) -> None:
         tabs_in_line = line.count('\t')
         # print(f"tabs_in_line: {tabs_in_line}; tabs_in_record: {tabs_in_record}")
 
+        line_no_initial = line_no
         while not eof and tabs_in_line < tabs_in_record:
-            print(f"< Line number {line_no} has {tabs_in_line} fields but wanted {tabs_in_record}, last character {ord(line[-1])}")
+            print(f"< Line number {line_no_initial}:{line_no} has {tabs_in_line} fields but wanted {tabs_in_record}, last character {ord(line[-1])}")
             bad_no += 1
             line_no += 1
             next_line = file_r.readline()
@@ -72,9 +78,13 @@ def check(ifilename: str) -> None:
 
     print(f"Lines in file: {line_no}, bad records: {bad_no}")
     file_r.close()
+    if bad_no == 0:
+        return 0
+    return 1
 
 
 def fix_records_to_have_same_number_of_fields(ifilename: str, ofilename: str) -> None:
+    print(f"Fixing file {ifilename}, saving to file {ofilename}...")
     file_w = open(ofilename, 'w')
     file_r = open(ifilename, 'r')
 
@@ -96,8 +106,9 @@ def fix_records_to_have_same_number_of_fields(ifilename: str, ofilename: str) ->
             break
         tabs_in_line = line.count('\t')
 
+        line_no_initial = line_no
         while not eof and tabs_in_line < tabs_in_record:
-            print(f"< Line number {line_no} has {tabs_in_line} fields but wanted {tabs_in_record}, last character {ord(line[-1])}")
+            print(f"< Line number {line_no_initial}:{line_no} has {tabs_in_line} fields but wanted {tabs_in_record}, last character {ord(line[-1])}")
             # This should be a new line character, so remove it
             line = line[:-1]
             line_no += 1
@@ -118,13 +129,44 @@ def fix_records_to_have_same_number_of_fields(ifilename: str, ofilename: str) ->
     file_r.close()
 
 
-print(f"Processing input file {args.filename}")
+def all(dir: str) -> int:
+    if not os.path.isdir(dir):
+        print(f"Filename when using --all must be a directory")
+        exit(1)
+    print(f"Checkinging all OWLNETS_node_metadata.txt files in the directory {dir}")
+    check_errors = 0
+    for dir2 in os.listdir(dir):
+        dir2_path = os.path.join(dir, dir2)
+        if os.path.isdir(dir2_path):
+            file_path = os.path.join(dir2_path, 'OWLNETS_node_metadata.txt')
+            if not os.path.isfile(file_path):
+                print(f"ERROR: {file_path} not found or not a file?")
+                continue
+            if check(file_path) == 0:
+                print(f"File {file_path} is well formed, skipping fix...")
+                continue
+            print(f"File {file_path} contains errors fixing...")
+            file_orig_path = os.path.join(dir2_path, 'OWLNETS_node_metadata_orig.txt')
+            os.rename(file_path, file_orig_path)
+            fix_records_to_have_same_number_of_fields(file_orig_path, file_path)
+            check_exit_status = check(file_path)
+            if check_exit_status != 0:
+                print(f"ERROR: {file_path} check after fix revealed errors?!")
+                check_errors += 1
+    if check_errors == 0:
+        return 0
+    return 1
 
+
+print(f"Processing {args.filename}")
+
+exit_status = 0
 if args.fix is not None:
-    print("Fixing file...")
     fix_records_to_have_same_number_of_fields(args.filename, args.fix)
-if args.check is True:
-    print("Checking file...")
-    check(args.filename)
+if args.check is True or args.fix is True:
+    exit_status = check(args.filename)
+if args.all is True:
+    exit_status = all(args.filename)
 
 print('Done!')
+exit(exit_status)

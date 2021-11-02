b3fbde520a553b6f5db42f308f3566c0aeae52bf
ChuckKollar <chuck@kollar.com>
2021-11-02 15:17:32 -0400

    Updated text in help

--- server/hu-bmap-ontology-api-client/terms_term_id_terms.py
+++ server/hu-bmap-ontology-api-client/terms_term_id_terms.py
@@ -27,7 +27,7 @@ parser.add_argument("-t", '--term_id', type=str,
 parser.add_argument("-s", '--sep', type=str, default=',',
                     help='separator to use between to delineate attributes in object while printing')
 parser.add_argument("-v", "--verbose", action="store_true",
-                    help='increase output verbosity')
+                    help='increase output verbosity writing to stderr')
 
 args = parser.parse_args()
 

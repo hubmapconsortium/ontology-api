8f891d0dd8dcd15bf323f596df0459bb5376fb83
AlanSimmons <AlanSimmons@users.noreply.github.com>
2022-08-16 15:23:09 -0400

    Additional parameterization; increased validation; revised readme file.

--- scripts/skowlnets/README.md
+++ scripts/skowlnets/README.md
@@ -4,8 +4,11 @@ Uses the input spreadsheet for the SimpleKnowledge Editor to generate a set of t
 
 User guide to build the SimpleKnowledge Editor spreadsheet: [https://docs.google.com/document/d/1wjsOzJYRV2FRehX7NQI74ZKRXvH45l0QmClBBF_VypM/edit?usp=sharing]
 
-## Assumption
-The script expects a file in the application directory named SimpleKnowledgeHuBMAP.xlsx.
+## Arguments
+1. The name of the SimpleKnowledge input spreadsheet for the ontology (e.g., SimpleKnowledgeHuBMAP.xlsx)
+2. The name for the ontology.
+
+The script will look for a file in the application directory with a name that matches the first argument.
 
 ## Format of SimpleKnowledge Editor spreadsheet
 
@@ -15,8 +18,14 @@ The script expects a file in the application directory named SimpleKnowledgeHuBM
 - Column D: pipe-delimited list of synonyms
 - Column E: pipe-delimited list of references to other ontologies. 
 
-Columns after F describe relationships. Column F corresponds to *isa*.
+Column F corresponds to an *isa* relationship. 
+Columns after F describe custom relationships. 
 
 Each cell in a relationship column contains a comma-delimited list of object concepts that relate.
+Each of the object concepts should be defined in a row in the spreadsheet (in column A). 
+
+The SimpleKnowledge spreadsheet's input validation does not capture differences
+in case--e.g., if a relationship cell refers to "Abc", when the actual
+term is "ABC". The script validates for these cases.
 
 The script loops through each relationship cell in a row and creates a set of subject-predicate-object relationships between the concepts in the cell and the subject concept in column B.
--- scripts/skowlnets/skowlnets.py
+++ scripts/skowlnets/skowlnets.py
@@ -3,111 +3,123 @@
 
 # # SimpleKnowledge to OWLNETS converter
 # 
-# Uses the input spreadsheet for the SimpleKnowledge Editor to generate a set of text files that comply with the OWLNETS format, as described in https://github.com/callahantiff/PheKnowLator/blob/master/notebooks/OWLNETS_Example_Application.ipynb.
+# Uses the input spreadsheet for the SimpleKnowledge Editor to generate a set of text files that comply with the
+# OWLNETS format, as described in https://github.com/callahantiff/PheKnowLator/blob/master/notebooks
+# /OWLNETS_Example_Application.ipynb.
 # 
-# User guide to build the SimpleKnowledge Editor spreadsheet: https://docs.google.com/document/d/1wjsOzJYRV2FRehX7NQI74ZKRXvH45l0QmClBBF_VypM/edit?usp=sharing
+# User guide to build the SimpleKnowledge Editor spreadsheet:
+# https://docs.google.com/document/d/1wjsOzJYRV2FRehX7NQI74ZKRXvH45l0QmClBBF_VypM/edit?usp=sharing
 
-# In[2]:
+# SimpleKnowledge to OWLNETS converter
 
+# Uses the input spreadsheet for the SimpleKnowledge Editor to generate text files that comply with the OWLNETS format.
 
-#!/usr/bin/env python
-# coding: utf-8
-
-#SimpleKnowledge to OWLNETS converter
-
-#Uses the input spreadsheet for the SimpleKnowledge Editor to generate text files that comply with the OWLNETS format.
-
-#TO DO: parameterize input file name.
-
-SIMPLEKNOWLEDGE_FILE: str = './SimpleKnowledgeHuBMAP.xlsx'
-
+import argparse
 import sys
 import pandas as pd
 import numpy as np
 import os
 
 
-# In[8]:
+# Parse an argument that identifies the version of the UMLS in Neptune from which to build
+# the CSV files.
+class RawTextArgumentDefaultsHelpFormatter(
+    argparse.ArgumentDefaultsHelpFormatter,
+    argparse.RawTextHelpFormatter
+):
+    pass
 
 
-#Read input spreadsheet.
+parser = argparse.ArgumentParser(
+    description='Builds ontology files in OWLNETS format from a spreadsheet in SimpleKnowledge format.',
+    formatter_class=RawTextArgumentDefaultsHelpFormatter)
+parser.add_argument("skfile", help="SimpleKnowledge spreadsheet name")
+parser.add_argument("ontology", help="Name of ontology")
+args = parser.parse_args()
 
-df_sk = pd.read_excel(SIMPLEKNOWLEDGE_FILE,sheet_name='SimpleKnowledgeEditor')
+# Read input spreadsheet.
+try:
+    df_sk = pd.read_excel(args.skfile, sheet_name='SimpleKnowledgeEditor')
+except FileNotFoundError:
+    err = 'Missing input spreadsheet: ' + args.skfile
+    raise SystemExit(err)
 
-#Build OWLNETS text files.
+# Build OWLNETS text files.
+# The OWLNETS format represents ontology data in a TSV in format:
 
-
-#EDGE (subject-predicate-object) DATA
-
-#subject <tab> predicate <tab> object
-#subject: code in HuBMAP ontology
-#predicate: relationship
+# subject <tab> predicate <tab> object
+#
+# where:
+#   subject - code for node in custom ontology
+#   predicate - relationship
+#   object: another code in the custom ontology
 #
 #  (In canonical OWLNETS, the relationship is a URI for a relation 
-#  property in a standard OBO ontology, such as RO.) For the HuBMAP
-#  ontology, we use custom relationship strings.)
-
-#object: code in HuBMAP ontology
+#  property in a standard OBO ontology, such as RO.) For custom
+#  ontologies such as HuBMAP, we use custom relationship strings.)
 
 edge_list_filename = './OWLNETS_edgelist.txt'
-
-with open(edge_list_filename,'w') as out:
+print('Building OWLNETS_edge')
+with open(edge_list_filename, 'w') as out:
     out.write('subject' + '\t' + 'predicate' + '\t' + 'object' + '\n')
 
-    #Each column after E in the spreadsheet (isa, etc.) represents a type of 
-    #subject-predicate_object relationship.
+    # Each column after E in the spreadsheet (isa, etc.) represents a type of
+    # subject-predicate_object relationship.
     #   1. Column F represents the isa relationship. 
     #   2. Columns after F represent relationships other than isa.
-    
-    #A relationship is represented in an OWLNETS ontology file with a URI that corresponds
-    #to a relationship property in an OBO ontology, such as RO--e.g.,
-    #contains (http://purl.obolibrary.org/obo/RO_0001019). 
-    #   1. For Column F (isa), the relationship is set to 'subClassOf'.
-    #   2. For other columns, the relationship property is stored in the column header.
-    
-    #Cells in relationship columns contain comma-separated lists of object nodes. 
-    
-    #Thus, a relationship cell, in general, represents a set of subject-predicate-object
-    #relationships between the concept in the "code" cell and the concepts in the relationship
-    #cell.
-      
+
+    # Cells in relationship columns contain comma-separated lists of object nodes.
+
+    # Thus, a relationship cell, in general, represents a set of subject-predicate-object
+    # relationships between the concept in the "code" cell and the concepts in the relationship
+    # cell.
+
     for index, row in df_sk.iterrows():
-        if index > 0: #non-header
+
+        if index > 0:  # non-header
             subject = str(row['code'])
-            
-            for col in range(5,len(row)):
-                #Obtain relationship.
+
+            for col in range(5, len(row)):
+                # Obtain relationship.
                 if col == 5:
                     predicate_uri = 'isa'
                 else:
                     colhead = df_sk.columns[col]
-                    #predicate_uri = colhead[colhead.find('(')+1:colhead.find(')')]
+                    # predicate_uri = colhead[colhead.find('(')+1:colhead.find(')')]
                     predicate_uri = colhead
-                    
-                #Obtain codes in the proposed ontology for object concepts involved
-                #in subject-predicate-object relationships.
+
+                # Obtain codes in the proposed ontology for object concepts involved
+                # in subject-predicate-object relationships.
                 objects = row.iloc[col]
-                
+
                 if not pd.isna(objects):
-                    
+
                     listobjects = objects.split(',')
-                    
+
                     for obj in listobjects:
-                        #Match object terms with their respective codes (Column A), 
-                        #which will result in a dataframe of one row.
-                        objcode = df_sk[df_sk['term']==obj].iloc[0,1]
-                        out.write(subject + '\t' + predicate_uri + '\t' + objcode + '\n')
-    
-     
-#NODE METADATA
-#Write a row for each unique concept in in the 'code' column.
+                        # Match object terms with their respective codes (Column A),
+                        # which will result in a dataframe of one row.
+                        match = df_sk[df_sk['term'] == obj]
+                        if match.size == 0:
+                            err = 'Error: row for \'' + subject + '\' indicates relationship \'' + predicate_uri
+                            err = err + '\' with node \'' + obj + '\', but this node is not defined in the \'term\' '
+                            err = err + 'column. (Check for spelling and case of node name.)'
+                            raise SystemExit(err)
+
+                        objcode = match.iloc[0, 1]
+                        out.write(subject + '\t' + predicate_uri + '\t' + str(objcode) + '\n')
+
+# NODE METADATA
+# Write a row for each unique concept in in the 'code' column.
 
 node_metadata_filename = './OWLNETS_node_metadata.txt'
-with open(node_metadata_filename,'w') as out:
-    out.write('node_id' + '\t' + 'node_namespace' + '\t' + 'node_label' + '\t' + 'node_definition' + '\t' + 'node_synonyms' + '\t' + 'node_dbxrefs' + '\n')
+print('Building OWLNETS_node_metadata')
+with open(node_metadata_filename, 'w') as out:
+    out.write(
+        'node_id' + '\t' + 'node_namespace' + '\t' + 'node_label' + '\t' + 'node_definition' + '\t' + 'node_synonyms' + '\t' + 'node_dbxrefs' + '\n')
 
     for index, row in df_sk.iterrows():
-        if index > 0: #non-header
+        if index > 0:  # non-header
             node_id = str(row['code'])
             node_namespace = 'HubMAP'
             node_label = row['term']
@@ -118,44 +130,26 @@ with open(node_metadata_filename,'w') as out:
             node_dbxrefs = str(row['dbxrefs'])
             if not pd.isna(node_dbxrefs):
                 node_dbxrefs = ''
-                
-            out.write(node_id + '\t' + node_namespace + '\t' + node_label + '\t' + node_definition + '\t' + node_synonyms + '\t' + node_dbxrefs + '\n')
 
-            
-#RELATION METADATA
-#Create a row for each type of relationship.
+            out.write(
+                node_id + '\t' + node_namespace + '\t' + node_label + '\t' + node_definition + '\t' + node_synonyms + '\t' + node_dbxrefs + '\n')
+
+# RELATION METADATA
+# Create a row for each type of relationship.
 
 relation_filename = './OWLNETS_relations.txt'
-with open(relation_filename,'w') as out:
-    out.write('relation_id' + '\t' + 'relation_namespace' + '\t' + 'relation_label' + '\t' + 'relation_definition' + '\n')
-    
-    for col in range(6,len(df_sk.columns)):
+print('Building OWLNETS_node_metadata')
+with open(relation_filename, 'w') as out:
+    out.write(
+        'relation_id' + '\t' + 'relation_namespace' + '\t' + 'relation_label' + '\t' + 'relation_definition' + '\n')
+
+    for col in range(6, len(df_sk.columns)):
         colhead = df_sk.columns[col]
-        #predicate_uri = colhead[colhead.find('(')+1:colhead.find(')')]
+        # predicate_uri = colhead[colhead.find('(')+1:colhead.find(')')]
         predicate_uri = colhead
-        
-        relation_namespace = 'HuBMAP'
-        
-        """label = colhead[0:colhead.find('(')]
-        #Parse the namespace, which depends on the ontology URI.
-        if predicate_uri.find('ccf') > 0:
-            relation_namespace = 'CCF'
-        elif predicate_uri.find('CHMO') > 0:
-            relation_namespace = 'CHMO'
-        elif predicate_uri.find('snomed') > 0:
-            relation_namespace = 'SNOMEDCT'
-        else:
-            relation_namespace = predicate_uri.split('/')[-1]
-            relation_namespace = relation_namespace.replace('#','_')
-            relation_namespace = relation_namespace.split('_')[0].upper()
-        """
+
+        relation_namespace = args.ontology
+
         relation_definition = ''
-        #out.write(predicate_uri + '\t' + relation_namespace + '\t' + label + '\t' + relation_definition + '\n')
+        # out.write(predicate_uri + '\t' + relation_namespace + '\t' + label + '\t' + relation_definition + '\n')
         out.write(predicate_uri + '\t' + relation_namespace + '\t' + predicate_uri + '\t' + relation_definition + '\n')
-
-
-# In[ ]:
-
-
-
-
